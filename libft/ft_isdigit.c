03048a33627cc9cca788d4d79184c591c06235c5
Nicole Mantilla Fernandez <nmantill@c3r4s1.42malaga.com>
2024-12-06 20:56:26 +0100

    add function ft_isdigit

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,12 +6,21 @@
 /*   By: nmantill <nmantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 22:07:40 by nmantill          #+#    #+#             */
-/*   Updated: 2024/12/05 22:14:47 by nmantill         ###   ########.fr       */
+/*   Updated: 2024/12/06 20:55:33 by nmantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <stdio.h>
+
 int	ft_isdigit(int c)
 {
-	if((c >= 48 && c <= 57) || ());
-	return(1)
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
 }
+// int main(void)
+// {
+// 	int	result = ft_isdigit('2');
+// 	printf("El resultado es:%i.\n", result);
+// 	return(0);
+// }
