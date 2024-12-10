880c1b524711b25490d69b00748b3a8bdc78b8e3
Nicole Mantilla Fernandez <nmantill@c1r12s3.42malaga.com>
2024-12-10 20:42:58 +0100

    add function ft_isascii

---
+++ libft/ft_isascii.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmantill <nmantill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 20:31:02 by nmantill          #+#    #+#             */
+/*   Updated: 2024/12/10 20:42:09 by nmantill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	int	result = ft_isascii(140);
+	printf("El resultado es:%i.\n", result);
+	return(0);
+}
