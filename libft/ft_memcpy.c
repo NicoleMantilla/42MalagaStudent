2855ee4b47d2d15a7643e20d434ee61a5dc209dc
Nicole Mantilla Fernandez <nmantill@c3r10s3.42malaga.com>
2024-12-16 22:07:15 +0100

    add ft_memcpy & ft_bzero

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmantill <nmantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/16 21:29:48 by nmantill          #+#    #+#             */
-/*   Updated: 2024/12/16 21:40:18 by nmantill         ###   ########.fr       */
+/*   Created: 2024/12/16 21:46:01 by nmantill          #+#    #+#             */
+/*   Updated: 2024/12/16 22:05:53 by nmantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*pntr;
+	char	*pntr_d;
+	const unsigned char	*pntr_s;
 
-	pntr = (unsigned char *) s;
+	*pntr_d = dest;
+	*pntr_s = src;
 
-	while (len > 0)
+	while (n-- >)
 	{
-		*(pntr++) = 0;
-		n--;
+		*(pntr_d++) = *pntr_s++;
+		return (dst);
 	}
 }
-
