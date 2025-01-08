2456f9b6313b44d58d00a588f02526f43f25d844
Nicole Manfer <nicolemf14@gmail.com>
2025-01-08 20:25:46 +0100

    correct substr

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmantill <nmantill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nicolemantillafernandez <nicolemantilla    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 14:05:02 by nicolemanti       #+#    #+#             */
-/*   Updated: 2025/01/07 20:55:29 by nmantill         ###   ########.fr       */
+/*   Updated: 2025/01/08 19:47:17 by nicolemanti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@ char	*ft_strtrim(char const *s1, char const *set)
 	const char	*start;
 	const char	*end;
 	char		*d;
+	size_t		len;
 
 	if (!s1 || !set)
 		return (NULL);
 	start = s1;
-	end = s1 + ft_strlen(s1) - 1;
 	while (*start && ft_strchr(set, *start))
 		start++;
+	end = s1 + ft_strlen(s1) - 1;
 	while (end > start && ft_strchr(set, *end))
 		end--;
-	d = (char *)malloc(end - start);
+	len = end - start + 1;
+	d = (char *)malloc(len + 1);
 	if (!d)
 		return (NULL);
-	ft_memcpy(d, start, end - start + 1);
-	d[end - start + 1] = '\0';
+	ft_memcpy(d, start, len);
+	d[len] = '\0';
 	return (d);
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nicolemantillafernandez <nicolemantilla    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 13:37:51 by nicolemanti       #+#    #+#             */
-/*   Updated: 2025/01/04 19:02:39 by nicolemanti      ###   ########.fr       */
+/*   Updated: 2025/01/08 20:11:46 by nicolemanti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	j;
 	size_t	i;
+	size_t str_len;
 	char	*sbstr;
 
 	if (!s)
 		return (NULL);
-	if (start >= (unsigned int)ft_strlen(s))
+	str_len = ft_strlen(s);
+	if (start >= str_len)
 		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len -start;
 	sbstr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!sbstr)
 		return (NULL);
